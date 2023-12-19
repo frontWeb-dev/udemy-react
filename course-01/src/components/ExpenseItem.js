38b648c299d2284ef7cad2fe3b25d91b0e7bf710
frontweb-dev <frontend_web@kakao.com>
2023-12-19 17:21:45 +0900

    feat: props를 통해 데이터 전달하기

--- course-01/src/components/ExpenseItem.js
+++ course-01/src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2023, 11, 19);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
-
+const ExpenseItem = ({ expense }) => {
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toDateString()}</div>
+      <div>{expense.date.toDateString()}</div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+        <h2>{expense.title}</h2>
+        <div className='expense-item__price'>${expense.amout}</div>
       </div>
     </div>
   );
