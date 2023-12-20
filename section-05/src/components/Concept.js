627212ecdd4cc3d4d81d1fa45181e1f8bd2590b2
frontweb-dev <frontend_web@kakao.com>
2023-12-20 23:15:53 +0900

    연습 프로젝트

---
+++ section-05/src/components/Concept.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../index.css';
+
+const Concept = ({ props }) => {
+  return (
+    <ul id='concepts'>
+      <li className='concept'>
+        <img src={props.image} alt={props.title} />
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
+      </li>
+    </ul>
+  );
+};
+
+export default Concept;
