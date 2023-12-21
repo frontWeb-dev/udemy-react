9e6a90b1efd3f2970ae06bb3bae58dbfba0b63d1
frontweb-dev <frontend_web@kakao.com>
2023-12-22 00:03:35 +0900

    양식 입력 추가하기

--- section-04/src/App.js
+++ section-04/src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   const expenses = [
@@ -27,6 +28,8 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <Expenses items={expenses} />
+
+      <NewExpense />
     </div>
   );
 }
