b3424f74e959c5288eee6a9ae7e29e28c913d0da
frontweb-dev <frontend_web@kakao.com>
2024-01-01 20:08:02 +0900

    공유 핸들러 함수 생성하기

--- section-04/src/components/NewExpense/ExpenseForm.js
+++ section-04/src/components/NewExpense/ExpenseForm.js
@@ -1,25 +1,45 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-  const [input, setInput] = useState({ title: '', amount: '', date: '' });
+  // const [input, setInput] = useState({ title: '', amount: '', date: '' });
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
 
-  const changeHandler = (e) => {
-    const type = e.target.type;
+  const titleChangeHandler = (e) => {
+    setTitle(e.target.value);
+    // setInput({...input, title: e.target.value})
+    // setInput((prev) => {
+    //   return { ...prev, title: e.target.value };
+    // });
+  };
 
-    if (type === 'text')
-      setInput((prev) => {
-        return { ...prev, title: e.target.value };
-      });
-    else if (type === 'number')
-      setInput((prev) => {
-        return { ...prev, amount: e.target.value };
-      });
-    else if (type === 'date')
-      setInput((prev) => {
-        return { ...prev, date: e.target.value };
-      });
+  const amountChangeHandler = (e) => {
+    setAmount(e.target.value);
+    // setInput({...input, amount: e.target.value})
+    // setInput((prev) => {
+    //   return { ...prev, amount: e.target.value };
+    // });
+  };
+
+  const dateChangeHandler = (e) => {
+    setDate(e.target.value);
+    // setInput({...input, date: e.target.value})
+    // setInput((prev) => {
+    //   return { ...prev, date: e.target.value };
+    // });
+  };
+
+  const inputChangeHandler = (identifier, value) => {
+    if (identifier === 'title') {
+      setTitle(value);
+    } else if (identifier === 'amount') {
+      setAmount(value);
+    } else {
+      setDate(value);
+    }
   };
 
   return (
@@ -27,17 +47,30 @@ const ExpenseForm = () => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={changeHandler} />
+          <input
+            type='text'
+            onChange={(event) => inputChangeHandler('title', event.target.value)}
+          />
         </div>
 
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' onChange={changeHandler} />
+          <input
+            type='number'
+            min='0.01'
+            step='0.01'
+            onChange={(event) => inputChangeHandler('amount', event.target.value)}
+          />
         </div>
 
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2023-01-01' max='2024-12-31' onChange={changeHandler} />
+          <input
+            type='date'
+            min='2023-01-01'
+            max='2024-12-31'
+            onChange={(event) => inputChangeHandler('date', event.target.value)}
+          />
         </div>
       </div>
       <div className='new-expense__actions'>
