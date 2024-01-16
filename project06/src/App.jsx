7247c3165bd5e1f0bf1d7f8787ae9527d9529efe
frontweb-dev <frontend_web@kakao.com>
2024-01-16 22:37:08 +0900

    틱택토 게임 첫 단계

--- project06/src/App.jsx
+++ project06/src/App.jsx
@@ -1,5 +1,20 @@
 function App() {
-  return <div></div>;
+  return (
+    <main>
+      <div id='game-container'>
+        <ol id='players'>
+          <li>
+            <span className='player-name'>Player 1</span>
+            <span className='player-symbol'>X</span>
+          </li>
+          <li>
+            <span className='player-name'>Player 2</span>
+            <span className='player-symbol'>O</span>
+          </li>
+        </ol>
+      </div>
+    </main>
+  );
 }
 
 export default App;
