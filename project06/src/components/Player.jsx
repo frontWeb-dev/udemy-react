2facadc3ded9e7e7ace12a125d299652ad7d490f
frontweb-dev <frontend_web@kakao.com>
2024-01-16 23:14:23 +0900

    State 활용법

--- project06/src/components/Player.jsx
+++ project06/src/components/Player.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Player = ({ name, symbol, ...props }) => {
+const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
+
+  const clickHandler = () => {
+    setIsEditing(!isEditing);
+  };
+
+  const changeHandler = (e) => {
+    setPlayerName(e.target.value);
+  };
+
   return (
     <li>
       <span className='player'>
-        <span className='player-name'>{name}</span>
+        {isEditing ? (
+          <input type='text' onChange={changeHandler} />
+        ) : (
+          <span className='player-name'>{playerName}</span>
+        )}
         <span className='player-symbol'>{symbol}</span>
       </span>
-      <button {...props}>Edit</button>
+      <button onClick={clickHandler}>{isEditing ? 'Change' : 'Edit'}</button>
     </li>
   );
 };
